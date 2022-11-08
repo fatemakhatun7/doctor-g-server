33c4fc2defea1c1d10461572263526afb6847dd3
fatemakhatun7 <fatema.sprdnc@gmail.com>
2022-11-08 20:59:32 +0600

    mongodb-user-added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require ('jsonwebtoken');
 require('dotenv').config();
 
@@ -11,6 +12,11 @@ app.use(express.json());
 app.get('/', (req, res)=> {
     res.send("Doctor G server is running on port 5000");
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fxdma7b.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.listen(port, ()=>{
     console.log(`Doctor G server is running on port ${port}`)
